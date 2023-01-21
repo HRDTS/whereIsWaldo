4a267f637d7a435087a6d2ff3c1b7bcb5e960aa6
Tunahan <t.genc58@hotmail.com>
2023-01-21 21:17:00 +0100

    fixed image placement

--- where-is-waldo/src/background.js
+++ where-is-waldo/src/background.js
@@ -1,13 +1,18 @@
 import React from "react";
 import WIW from './img/whereIsWaldoBackground.jpg'
 import Odlaw from './img/Character.Odlaw.webp'
+import Charmander from './img/Character.Charmander.png'
+import Pikachu from './img/Character.Pikachu2.png'
+import Psyduck from './img/Character.Psyduck2.png'
 
 function Background() {
 
     return (
         <div className="backgroundDiv">
             <img className="backgroundImage" src={WIW}/>
-            <img className="odlaw" src={Odlaw}/>
+            <img className="charmander" src={Charmander}/>
+            <img className="pikachu" src={Pikachu}/>
+            <img className="psyduck" src={Psyduck}/>
         </div>
 
     )
