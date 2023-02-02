8e729e5f75ffa9ba0e50824b2d4e3466d525dcbf
Tunahan <t.genc58@hotmail.com>
2023-02-02 20:33:28 +0100

    added styling and structure to home page, and fixed scoreboard (including the high to low sorting)

--- where-is-waldo/src/home.js
+++ where-is-waldo/src/home.js
@@ -1,86 +1,61 @@
 import React, { useEffect, useState } from "react";
 import PokeMap from './img/pokemon3.jpg';
+import Tangela from './img/Character.Tangela.png'
+import Diglet from './img/Character.Diglet.png'
+import Scyther from './img/Character.Scyther.png'
+import Logo from './img/Logo.Waldo.svg'
 
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { firestore } from "./firebase";
-
-// {doc.data().date.toDate().toISOString().slice(0, 10).split('-').reverse().join('/')}
-
-
-async function fetchScoreboard() {
-    try {
-        const querySnapshot = await getDocs(collection(firestore, 'users'))
-        const wholeData = []
-
-        querySnapshot.forEach((doc) => {
-            wholeData.push(doc.data())
-        })
-
-        const listItems = wholeData.map((data) => {
-            <li>{data}</li>
-        })
-        return wholeData
-    }
-    catch(error) {
-        console.log(`could not fetch scoreboard: ${error}`)
-    }
-}
-
-function Render () {
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetchScoreboard()
-        .then(data => setData(data))
-    }, [])
-
-    const listData = data.map((element) => 
-    <tr>
-        1
-        {element.username}
-        {element.time}
-        {element.date.toDate().toISOString().slice(0, 10).split('-').reverse().join('/')}
-    </tr>
-    )
-    return (
-        listData
-    )
-
-}
-    
 
 function Home() {
 
     return (
         <div className="homePage">
-            <div className="homePageGrid">
-                <img src={PokeMap} className='homePageImage'/>
-                <img src={PokeMap} className='homePageImage'/>
-                <img src={PokeMap} className='homePageImage'/>
-                
-                    <tr>
-                        <th>place</th>
-                        <th>name</th>
-                        <th>time</th>
-                        <th>date</th>
-                    </tr>
-                    <Render/>
+            <header className="homePageHeader"> <img className="homePageLogo" src={Logo}/> </header>
+            <div className="homePageBody">
+
+                <div className="homePageGrid">
                     
-                    <tr>
-                        <th>place</th>
-                        <th>name</th>
-                        <th>time</th>
-                        <th>date</th>
-                    </tr>
+                <div className="homePageDifficulty">Waldo x Pokemon</div>
+                <div className="homePageDifficulty">Waldo x Digimon</div>
+                <div className="homePageDifficulty">Waldo x Sony</div>
 
+                    <div className="homePageCharacterDiv">
+                    <div className="homePageDifficulty">level 1:</div>
+                        <img className="homePageCharacter" src={Tangela}/>
+                        <img className="homePageCharacter" src={Diglet}/>
+                        <img className="homePageCharacter" src={Scyther}/>
+                    </div>
 
+                    <div className="homePageCharacterDiv">
+                    <div className="homePageDifficulty">level 2:</div>
+                        <img className="homePageCharacter" src={Tangela}/>
+                        <img className="homePageCharacter" src={Diglet}/>
+                        <img className="homePageCharacter" src={Scyther}/>
+                    </div>
 
-                <div className="map2Ranking">hello world</div>
+                    <div className="homePageCharacterDiv">
+                    <div className="homePageDifficulty">level 3: </div>
+                        <img className="homePageCharacter" src={Tangela}/>
+                        <img className="homePageCharacter" src={Diglet}/>
+                        <img className="homePageCharacter" src={Scyther}/>
+                    </div>
 
-
-                <div className="map3Ranking">hello world</div>
+                    <div className="brighten"> <img src={PokeMap} className='homePageImage'/> </div>
+                    <div className="brighten"> <img src={PokeMap} className='homePageImage'/> </div>
+                    <div className="brighten"> <img src={PokeMap} className='homePageImage'/> </div>
+                </div>
+                <div  className="homePageScoreboardDiv"> 
+                    <div>
+                        <h1>Are you a true Waldo expert?</h1>
+                        <h1 className="homePageLeaderboardText">View the scoreboard</h1>
+                    </div>
+                    <button className="homePageScoreboardButton">scoreboard</button>
+                </div>
+                
             </div>
+
         </div>
+
     )
 }
 
---
+++ where-is-waldo/src/renderScoreboard2.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import PokeMap from './img/pokemon3.jpg';
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { firestore } from "./firebase";
+
+async function fetchScoreboard() {
+    try {
+        const querySnapshot = await getDocs(collection(firestore, 'users2'))
+        const wholeData = []
+
+        querySnapshot.forEach((doc) => {
+            wholeData.push(doc.data())
+        })
+
+        return wholeData
+    }
+    catch(error) {
+        console.log(`could not fetch scoreboard: ${error}`)
+    }
+}
+
+function RenderScoreboard2 () {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetchScoreboard()
+        .then(data => data.sort((a,b) => parseFloat(a.time) - parseFloat(b.time)))
+        .then(data => setData(data))
+    }, [])
+
+    const listDataUsername = data.map((element) => 
+    <tr>
+        {element.username}
+    </tr>)
+
+    const listDataTime = data.map((element) => 
+    <tr>
+    {element.time}
+    </tr>)
+
+    const listDataDate = data.map((element) => 
+    <tr>
+    {element.date.toDate().toISOString().slice(0, 10).split('-').reverse().join('/')}
+    </tr>)
+
+const listDataPlace = data.map((element, index) => 
+<tr>
+{index + 1}
+</tr>)
+
+    return (
+        <div>
+        <tr>
+            <th>place</th>
+            <th>name</th>
+            <th>time</th>
+            <th>date</th>
+        </tr>
+        <tr>
+            <td>{listDataPlace}</td>
+            <td>{listDataUsername}</td>
+            <td>{listDataTime}</td>
+            <td>{listDataDate}</td>
+        </tr>
+        </div>
+
+
+    )
+}
+
+export default RenderScoreboard2;
