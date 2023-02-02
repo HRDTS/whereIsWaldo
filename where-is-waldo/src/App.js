92fd2433c8d228fcd3f22a760a3b8aef558b0ebf
Tunahan <t.genc58@hotmail.com>
2023-02-02 20:46:50 +0100

    created scoreboard page with setup

--- where-is-waldo/src/App.js
+++ where-is-waldo/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Parent from "./parent";
 import Home from "./home";
+import Scoreboard from "./scoreboard";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Scoreboard/>
     </div>
   );
 }
--- where-is-waldo/src/scoreboard.js
+++ where-is-waldo/src/scoreboard.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { firestore } from "./firebase";
-
-
+import Logo from './img/Logo.Waldo.svg'
 
 function Scoreboard () {
 
 
+return (
+    <div>
+         <header className="homePageHeader2">
+            <span className="headerLeft">&nbsp;&nbsp;</span>
+             <img className="homePageLogo" src={Logo}/> 
+             <div className="headerRight">return to home page</div>
+             </header>
 
+             <div className="soreboardGrid">
+                <div className="scoreboardGridItem"></div>
+                <div className="scoreboardGridItem"></div>
+                <div className="scoreboardGridItem"></div>
+             </div>
+
+             <div>render scoreboard here based on state</div>
+    </div>
+)
 }
+
+export default Scoreboard;
