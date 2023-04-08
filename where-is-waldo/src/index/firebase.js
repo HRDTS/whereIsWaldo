0f7564820c6f3b8580eefd92ddfaaac15f8373e3
Tunahan <t.genc58@hotmail.com>
2023-04-08 17:52:40 +0200

    added env again

--- where-is-waldo/src/index/firebase.js
+++ where-is-waldo/src/index/firebase.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, onValue } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseApp = {
-    apiKey: REACT_APP_apiKey,
-    authDomain: REACT_APP_authDomain,
-    databaseURL: REACT_APP_databaseURL,
-    projectId: REACT_APP_projectId ,
-    storageBucket: REACT_APP_storageBucket,
-    messagingSenderId: REACT_APP_messagingSenderId,
-    appId: REACT_APP_appId,
-    measurementId: REACT_APP_measurementId,
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId ,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
   };
 
   const firebase = initializeApp(firebaseApp);
